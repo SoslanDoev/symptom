2263260b293897477dc0cfd28f3c7f1dbea4debc
SoslanDoev <soslan2002osetia@mail.ru>
2023-10-31 15:20:24 +0300

    v4

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  configureWebpack: {
+    plugins: [new MiniCssExtractPlugin()],
+  },
   publicPath: "/symptom/",
   transpileDependencies: true
 })
